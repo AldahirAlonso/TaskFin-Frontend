9139ea0da3658a459408b7eb2ee5a1689e465b09
Aldahir Alonso <aldahir0603@hotmail.com>
2026-02-02 23:08:22 -0600

    Creacion de Proyecto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=''>
+
+
+    <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    
+  return (
+    <header className="bg-gradient-to-r from-blue-400 to-purple-500 shadow p-10">
+      <h1 className="text-cyan-300 text-5xl font-bold">Bienvenido a <span className="text-teal-300">TaskFin</span> </h1>
+    </header>
+  );
+}
+
+export default Header;
