e57090edf1690cfefce711af1e7a3a3f524b01f1
Aldahir Alonso <aldahir0603@hotmail.com>
2026-02-09 18:18:16 -0600

    Avances Esteticos

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-function Header() {
-    
+function Sidebar() {
   return (
-    <header className="bg-gradient-to-r from-blue-400 to-purple-500 shadow p-10">
-      <h1 className="text-cyan-300 text-5xl font-bold">Bienvenido a <span className="text-teal-300">TaskFin</span> </h1>
-    </header>
+    <aside className="w-64 bg-gray-500 text-white h-screen p-10">
+      <h2 className="text-2xl font-bold mb-8">TaskFin</h2>
+
+      <nav className="flex flex-col gap-5">
+        <a href="#" className="hover:text-blue-400">Ver</a>
+        <a href="#" className="hover:text-blue-400">Crear</a>
+        <a href="#" className="hover:text-blue-400">Modificar</a>
+        <a href="#" className="hover:text-blue-400">Eliminar</a>
+      </nav>
+    </aside>
   );
 }
 
-
-export default Header;
+export default Sidebar;
